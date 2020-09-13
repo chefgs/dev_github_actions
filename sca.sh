9fc866fdb0eb2a835b8f178b7f0a1df280bf71ae
Saravanan G <g.gsaravanan@gmail.com>
2020-09-14 03:55:08 +0530

    cookstyle corrected

--- sca.sh
+++ sca.sh
@@ -3,5 +3,6 @@ ls -ltr | grep drw | grep -v cookbook | awk '{print $9}' | while read dir_name
 do
 # berks vendor ../cookbook_cache
 echo $dir_name
+cookstyle --auto-gen-config $dir_name
 cookstyle $dir_name
 done
