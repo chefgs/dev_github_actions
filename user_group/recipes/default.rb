70c1b9b8e1664719d91dbd05611c31d4fd7ea590
Saravanan <g.gsaravanan@gmail.com>
2020-09-16 16:38:07 +0000

    solving errors

--- user_group/recipes/default.rb
+++ user_group/recipes/default.rb
@@ -10,9 +10,11 @@ user 'bob' do
   group 'chefusers'
 end
 
-directory node['user_group']['file_path']
+directory node['user_group']['file_path'] do
+  recursive true
+end
 
-file_name = "node['user_group']['file_path']/chef.txt"
+file_name = "${node['user_group']['file_path']}/chef.txt"
 
 file file_name do
   content 'File is created by Chef file resource'
