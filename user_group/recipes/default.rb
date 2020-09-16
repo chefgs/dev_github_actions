1cd712b92271b0a071f12ff568eeacde57f21f31
Saravanan <g.gsaravanan@gmail.com>
2020-09-16 16:43:55 +0000

    solving errors

--- user_group/recipes/default.rb
+++ user_group/recipes/default.rb
@@ -14,7 +14,7 @@ directory '/tmp/cheftest/' do
   recursive true
 end
 
-file_name = "/tmp/cheftest/chef.txt"
+file_name = '/tmp/cheftest/chef.txt'
 
 file file_name do
   content 'File is created by Chef file resource'
