67d214026aba01fb287f1466e0ad99b64635a0a9
Saravanan <g.gsaravanan@gmail.com>
2020-09-16 17:48:16 +0000

    resolving error

--- user_group/spec/unit/recipes/default_spec.rb
+++ user_group/spec/unit/recipes/default_spec.rb
@@ -3,18 +3,19 @@
 # Spec:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
-require 'chefspec'
+require 'spec_helper'
 
 describe 'directory::create' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
-
+  #let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
+  platform 'ubuntu', '18.04'
   it 'Creates a directory with the default action' do
     expect(chef_run).to create_directory('/tmp/cheftest')
   end
 end
 
 describe 'file::create' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
+  #let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
+  platform 'ubuntu', '18.04'
 
   it 'Creates a file with an explicit action' do
     expect(chef_run).to create_file('/tmp/cheftest/chef.txt').with(
